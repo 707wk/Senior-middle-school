8c5852e10a2283e64301687d843d3aedaf2508c4
汪恳 <1174843091@qq.com>
2015-04-05 10:29:35 +0800

    Create data_ranking.h

---
+++ data_ranking/data_ranking.h
@@ -0,0 +1,11 @@
+#include "data_ranking.c"
+
+int check_data();
+int put_data_error();
+int put_data();
+int free_data();
+void data_exchange();
+int data_county_ranking();
+int data_school_ranking();
+int data_class_ranking();
+int data_read();
